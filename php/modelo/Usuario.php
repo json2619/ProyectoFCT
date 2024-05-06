129aaaff24fe3b4611ed258c4dcdfffb79988347
josemi <josemiguelf.19@hotmail.com>
2024-05-06 11:46:18 +0200

    Version 1.2 proyecto

---
+++ php/modelo/Usuario.php
@@ -0,0 +1,21 @@
+<?php
+class Usuario
+{
+    private $ID_Usuario;
+    private $Nombre;
+    private $FechNac;
+    private $Telefono;
+    private $Correo;
+    private $Contraseña;
+    private $Tipo_usuario;
+
+    public function __get($propiedad)
+    {
+        return $this->$propiedad;
+    }
+
+    public function __set($propiedad, $valor)
+    {
+        $this->$propiedad = $valor;
+    }
+}
